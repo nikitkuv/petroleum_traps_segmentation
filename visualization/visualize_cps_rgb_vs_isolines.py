8d22163117f1365e4594c45493f669b89c2d4cce
nikitkuv <nikitkuv@gmail.com>
2026-04-16 22:15:40 +0300

    add cps_tiles_dir in train.ipynb

--- visualization/visualize_cps_rgb_vs_isolines.py
+++ visualization/visualize_cps_rgb_vs_isolines.py
@@ -6,10 +6,15 @@ sys.path.append(str(Path(__file__).parent.parent))
 from visualization.visualize import overlay_isolines_on_rgb
 
 
+RGB_PATH: str = './data/images_cps/003_x_structuralNOisoline_U9_10_kolltop3.png'
+ISOLINES_PATH: str = './data/images_cps/003_x_isolines_U9_10_kolltop3.png'
+SAVE_PATH: str = './data/test_cps_visualization/overlay_003_U9_10_kolltop3.png'
+
+
 rgb, isolines, overlay = overlay_isolines_on_rgb(
-    rgb_path='./data/images_cps_full/x_structuralNOisoline_Ach3-2-1_toptop1.png',
-    isolines_path='./data/images_cps_full/x_isolines_Ach3-2-1_toptop1.png',
+    rgb_path=RGB_PATH, 
+    isolines_path=ISOLINES_PATH,
     alpha=0.5,
-    save_path='./data/test_cps_visualization/overlay_Ach3-2-1_toptop1.png',
+    save_path=SAVE_PATH,
     show=False
 )
